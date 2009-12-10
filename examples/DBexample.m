4f8feb1fbdc58d3952775e5917302ece02476d62
je16630 <je16630@ll.mit.edu>
2009-12-10 23:23:58 +0000

    simple examples

---
+++ examples/DBexample.m
@@ -0,0 +1,7 @@
+% Create a DB.
+DB = DBserver('f-2-1','cloudbase');
+
+% List the tables.
+
+tables = ls(DB);
+
---
+++ examples/ExampleInsert1.m
@@ -0,0 +1,19 @@
+% Example Insert
+%
+% host='localhost';
+% db='example_table';
+% rows ='1,2,3,4,5,6,7,8,9,10,'
+% cols='1,22,333,4444,55555,666666,7777777,88888888,999999999,10101010101010101010,'
+% vals='val1,val2,val3,val4,val5,val6,val7,val8,val9,val10,'
+%
+% DBinsert(db,rows,cols,vals);
+%
+
+%host='localhost';
+host='f-2-1.llgrid.ll.mit.edu';
+db='example_table1';
+rows ='1,2,3,4,5,6,7,8,9,10,'
+cols='1,22,333,4444,55555,666666,7777777,88888888,999999999,10101010101010101010,'
+vals='val1,val2,val3,val4,val5,val6,val7,val8,val9,val10,'
+
+DBinsert(host,db,rows,cols,vals);
