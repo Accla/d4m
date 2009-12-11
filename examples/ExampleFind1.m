5a96f1f4e8326e4349e9e5d18a960cc6cf17947c
je16630 <je16630@ll.mit.edu>
2009-12-11 16:24:03 +0000

    minor update

--- examples/ExampleFind1.m
+++ examples/ExampleFind1.m
@@ -14,3 +14,8 @@ rows ='1,2,3,8,9,10,'
 cols = '1,22,333,88888888,999999999,10101010101010101010,'
 
 [retRows,retCols,retVals]=DBsubsrefFind(host,db,rows,cols)
+
+DB = DBserver(host,cloudbase);
+T = DB(table);
+A = T(rows,cols);
+
