1c2d47c0f2aa2df8c5a929b0a3b387863b271f1e
wi20909 <wi20909@ll.mit.edu>
2010-01-12 15:55:38 +0000

    wgs

---
+++ examples/ExampleFindAll.m
@@ -0,0 +1,17 @@
+% Example Find All
+%
+% host='localhost'
+% db='example_table'
+% rows =':'
+% cols = ':'
+% [retRows,retCols,retVals]=DBsubsrefFind(host,db,rows,cols)
+%
+
+%host='localhost'
+host='f-2-1.llgrid.ll.mit.edu'
+db='example_table'
+rows =':'
+cols = ':'
+
+[retRows,retCols,retVals]=DBsubsrefFind(host,db,rows,cols)
+rows =':'
