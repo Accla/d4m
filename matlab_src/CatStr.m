93f8e1f7d7cb6cae6f563ff012b8ed756ed8b9fd
je16630 <je16630@ll.mit.edu>
2010-02-04 16:55:13 +0000

    concat to Strs

--- matlab_src/CatStr.m
+++ matlab_src/CatStr.m
@@ -1,11 +1,12 @@
 function s12 = CatStr(s1,sep,s2);
 %CATSTR cats 2 array of strings.
 
-  s1(s1 == s1(end)) = sep;
   s1mat = Str2mat(s1);
+  s1mat(s1mat == s1(end)) = sep;
   s2mat = Str2mat(s2);
-  [N N1] = size(s1mat); 
+  [N N1] = size(s1mat);
   [N N2] = size(s2mat);
+
   s12mat = zeros(2*N,max([N1 N2]));
   s12mat(1:2:end,1:N1) = s1mat;
   s12mat(2:2:end,1:N2) = s2mat;
