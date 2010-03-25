5f348a71688ea3d34a766c654eada3819fab18f6
cmcnally <cmcnally@ll.mit.edu>
2010-03-25 11:25:40 +0000

    minor mods for CB 1.1.0

--- bin/set-classpath.sh
+++ bin/set-classpath.sh
@@ -14,18 +14,19 @@ if [ -z "$BASEDIR" ]; then
   exit
 fi
 CLASSES=.
-CLASSES=$CLOUDBASE_HOME/conf/cloudbase-default.xml
 CLASSES="$CLASSES":$CLOUDBASE_HOME/conf/cloudbase-site.xml
-
+echo adding $CLOUDBASE_HOME/conf/cloudbase-site.xml
+CLASSES="$CLASSES":$CLOUDBASE_HOME/conf
+echo adding $CLOUDBASE_HOME/conf
 # CLASSES
 
 CLASSES_DIR="$BASEDIR"/lib
 #echo "classes dir  = $CLASSES_DIR"
 
 if [ -d "$CLASSES_DIR" ] ; then
-    for i in "$CLASSES_DIR"/*.*; do
+    for i in "$CLASSES_DIR"/*.jar; do
       CLASSES="$CLASSES":"$i"
-      
+		echo adding: $i
     done
 #echo "classes are = $CLASSES"
 fi
