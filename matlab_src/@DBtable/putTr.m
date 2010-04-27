f32719a258d3508b63494b2bc330a247a71dd218
je16630 <je16630@ll.mit.edu>
2010-04-27 20:31:07 +0000

    added putTr which inserts both table and its transpose

--- matlab_src/@DBtable/putTr.m
+++ matlab_src/@DBtable/putTr.m
@@ -1,5 +1,5 @@
-function T = put(T,A);
-%PUT inserts associative array in DB table.
+function T = putTr(T1,T2,A);
+%PUTTR inserts associative array and its transpose in DB tables.
   % Set chunk size in chars.
   chunkBytes = 5e5;
   M = nnz(A);
@@ -16,12 +16,14 @@ function T = put(T,A);
   totMax = rowMax + colMax + valMax;
   chunkSize = min(max(1,round(chunkBytes/totMax)),M);
 
-  DB = struct(T.DB);
+
+  DB = struct(T1.DB);
 
   for i=1:chunkSize:M
     i1 = min(i + chunkSize - 1,M);
     r = Mat2str(rowMat(i:i1,:)); c = Mat2str(colMat(i:i1,:));  v = Mat2str(valMat(i:i1,:));
-    DBinsert(DB.host, T.name, r, c, v );
+    DBinsert(DB.host, T1.name, r, c, v );
+    DBinsert(DB.host, T2.name, c, r, v );    % Insert transpose.
   end
 
 end
