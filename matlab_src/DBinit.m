a6d43e90e79727278bd9663689a59b33e4010250
je16630 <je16630@ll.mit.edu>
2010-07-10 16:04:13 +0000

    removed D4M_HOME

--- matlab_src/DBinit.m
+++ matlab_src/DBinit.m
@@ -1,5 +1,8 @@
 
-d4m_home=getenv('D4M_HOME');
+
+d4m_home = fileparts(fileparts(mfilename('fullpath')));
+
+%d4m_home=getenv('D4M_HOME');
 %addpath([d4m_home, '/matlab_src'])
 %addpath([d4m_home, '/examples'])
 %addpath([d4m_home, '/matlab_src/html'])
