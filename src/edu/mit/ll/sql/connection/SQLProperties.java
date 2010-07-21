ec3ce978acd3da7d66eaef596e98a71011efdb61
smohindra <smohindra@ll.mit.edu>
2010-07-21 22:04:13 +0000

    Updated SQLProperties.java

--- src/edu/mit/ll/sql/connection/SQLProperties.java
+++ src/edu/mit/ll/sql/connection/SQLProperties.java
@@ -1,171 +1,43 @@
 package edu.mit.ll.sql.connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Iterator;
-import java.util.SortedSet;
+import java.net.URL;
+import java.util.Properties;
 
-import org.apache.log4j.Logger;
+public class SQLProperties {
 
-public class SQLConnection {
+	private static Properties props;
 
-	private static final Logger logger = Logger.getLogger(SQLConnection.class);
-	public String databaseAddress = "jdbc:mysql://localhost:5432/someDb";
-	public String user = "user";
-	public String password = "pass";
-	public String driverName = "org.mysql.jdbc.Driver";
+	public static void init(String filename) {
+		props = new Properties();
 
-	private Statement statement;
-
-	protected Connection dbConnection;
-
-	/**
-	 * Construct the SQLConnection object
-	 * @param driverName
-	 * @param databaseAddress
-	 * @param user
-	 * @param password
-	 */
-	public SQLConnection(String driverName, String databaseAddress, String user, String password) {
-		if (driverName != null)
-			this.driverName = driverName;
-
-		if (databaseAddress != null)
-			this.databaseAddress = databaseAddress;
-
-		if (user != null)
-			this.user = user;
-
-		if (password != null)
-			this.password = password;
-	}
-
-	/**
-	 * TODO
-	 * @param host
-	 * @param userName
-	 * @param password2
-	 * @throws SQLException
-	 */
-	public SQLConnection(String host, String userName, String password2) throws SQLException{
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * Initiate a connection with the database -- throw a SQLException if error
-	 * occurs. If error occurs, the connection is null.
-	 */
-	public Connection connect() throws SQLException {
-		logger.info("Connecting to " + databaseAddress + " with user " + user + " and password " + password);
-		dbConnection = null;
-
+		// load data from the properties file
 		try {
-			// Load jdbc driver
-			Class.forName(driverName);
+			URL url = ClassLoader.getSystemResource(filename);
+			
+			if(url == null) 
+				url = Thread.currentThread().getContextClassLoader().getResource(filename);				
 
-			// Connect to database (using DriverManager)
-			dbConnection = DriverManager.getConnection(databaseAddress, user, password);
-		}
-		catch (ClassNotFoundException e) {
-			logger.error("Could not find driver: " + driverName, e);
-			dbConnection = null;
-		}
-
-		return dbConnection;
-	}
-
-	/**
-	 * Return current connection to database (without attempting to connect).
-	 */
-	public Connection getConnection() {
-		return dbConnection;
-	}
-
-	/**
-	 * Closes connection to database.
-	 */
-	public void close() {
-		try {
-			if (dbConnection != null)
-				dbConnection.close();
+			if(url == null)
+				url = SqlProperties.class.getClassLoader().getResource(filename);
+						
+			System.out.println("trying to load: " + url);
+			props.load(url.openStream());
+			System.out.println("just loaded: " + url);
 		}
 		catch (Exception e) {
-			logger.error(e);
-		}
-		finally {
-			dbConnection = null;
+			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * Execute the Query
-	 * @param sql	Query sting
-	 * @return		ResultSet
-	 * @throws SQLException
-	 */
-	public ResultSet exectueQuery(String sql) throws SQLException {
-		Statement statement = (Statement) dbConnection.createStatement();
-		ResultSet ret = (ResultSet) statement.executeQuery(sql);
-		statement.close();
-		return ret;
-	}
-
-	/**
-	 * Update
-	 * @param sql
-	 * @return
-	 * @throws SQLException
-	 */
-	public int exectueUpdate(String sql) throws SQLException {
-		Statement statement = (Statement) dbConnection.createStatement();
-		int ret = statement.executeUpdate(sql);
-		statement.close();
-		return ret;
-	}
-
-	/**
-	 * TODO 
-	 * @return
-	 * @throws SQLException
-	 */
-	public SortedSet getTableList() throws SQLException {
-		SortedSet ret = null;
-		return ret;
-	}
-	
-	/**
-	 * 
-	 * @param sql
-	 * @return
-	 * @throws SQLException
-	 */
-	public Object exectue(String sql) throws SQLException {
-		if (statement != null) {
-			statement.close();
+	public static Object get(Object key) {
+		// load the properties file if not already loaded
+		if (props == null) {
+			init("sql.properties");
 		}
-		statement = (Statement) dbConnection.createStatement();
-		boolean ret = statement.execute(sql);
 
-		if (ret)
-			return (ResultSet) statement.getResultSet();
-		else
-			return statement.getUpdateCount();
+		// return the value asked for
+		return props.get(key);
 	}
-
-	public void createTable(String tableName) throws SQLException{
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void deleteTable(String tableName) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-
-
 }
 
 /*
