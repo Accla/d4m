d87cd6eeacc260a032b93c1ae53fc2d55d630d7d
je16630 <je16630@ll.mit.edu>
2010-08-27 00:53:36 +0000

    minor mod

--- matlab_src/TEST/DBsetup.m
+++ matlab_src/TEST/DBsetup.m
@@ -1,6 +1,6 @@
 % Create a DB.
-DB = DBserver('f-2-8.llgrid.ll.mit.edu','cloudbase');
-DB2 = DBserver('f-2-8.llgrid.ll.mit.edu/f-2-9.llgrid.ll.mit.edu', 'cloudbase');
+DB = DBserver('f-2-9.llgrid.ll.mit.edu','cloudbase');
+DB2 = DBserver('f-2-9.llgrid.ll.mit.edu/f-2-9.llgrid.ll.mit.edu', 'cloudbase');
 %[stat,host] = system('hostname -s');
 %DB = DBserver([host(1:end-1) '.llgrid.ll.mit.edu'],'cloudbase');
 
