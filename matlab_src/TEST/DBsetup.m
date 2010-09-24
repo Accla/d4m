9affc20785b6ab3623fc52557165a1114f42a1ec
je16630 <je16630@ll.mit.edu>
2010-09-24 16:43:19 +0000

    hide pw

--- matlab_src/TEST/DBsetup.m
+++ matlab_src/TEST/DBsetup.m
@@ -1,8 +1,13 @@
+% Load conf.
+load -mat DBsetup.bin
+
 % Create a DB.
-%DB = DBserver('f-2-9.llgrid.ll.mit.edu','cloudbase');
-DB = DBserver('f-2-9.llgrid.ll.mit.edu', 'cloudbase', 'cloudbase', 'root', 'secret');
+%DB = DBserver('f-2-9.llgrid.ll.mit.edu', 'cloudbase', 'cloudbase', 'root', 'secret');
+DB = DBserver('f-2-9.llgrid.ll.mit.edu','cloudbase','cloudbase','root',password);
 %[stat,host] = system('hostname -s');
 %DB = DBserver([host(1:end-1) '.llgrid.ll.mit.edu'],'cloudbase');
+
+clear password
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % D4M: Dynamic Distributed Dimensional Data Model
