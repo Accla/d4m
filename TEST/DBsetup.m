9a20e400a972bce648f5963d1ac2bd31485390af
je16630 <je16630@ll.mit.edu>
2010-12-11 19:46:03 +0000

    minor tweak

--- TEST/DBsetup.m
+++ TEST/DBsetup.m
@@ -3,8 +3,8 @@ load -mat DBsetup.bin
 
 % Create a DB.  Should change instance name to something else.
 %DB = DBserver('f-2-8.llgrid.ll.mit.edu','BigTableLike','cloudbase','root',drowssap);   % 1.1
-%DB = DBserver('f-2-10.llgrid.ll.mit.edu','BigTableLike','cloudbase','root',drowssap);   % 1.2
-DB = DBserver('f-2-15.llgrid.ll.mit.edu','BigTableLike','cloudbase','root',drowssap);   % 1.2
+DB = DBserver('f-2-10.llgrid.ll.mit.edu','BigTableLike','cloudbase','root',drowssap);   % 1.2
+%DB = DBserver('f-2-15.llgrid.ll.mit.edu','BigTableLike','cloudbase','root',drowssap);   % 1.2
 %[stat,host] = system('hostname -s');
 %DB = DBserver([host(1:end-1) '.llgrid.ll.mit.edu'],'BigTableLike','cloudbase','root',drowssap); 
 
