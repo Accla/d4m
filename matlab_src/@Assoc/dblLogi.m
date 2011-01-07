68d9ccdd332386684629c33e6c686210ea19e537
je16630 <je16630@ll.mit.edu>
2011-01-07 22:59:08 +0000

    shorthand for double(logical())

---
+++ matlab_src/@Assoc/dblLogi.m
@@ -0,0 +1,16 @@
+function A = dblLogi(A)
+%DBLLOGI converts associative array adjacency matrix to double of ones.
+
+  A = double(logical(A));
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% D4M: Dynamic Distributed Dimensional Data Model
+% Architect: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% Software Engineer: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% MIT Lincoln Laboratory
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) <2010> Massachusetts Institute of Technology
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
