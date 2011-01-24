31f0a448073127f1e1e0bc06497fa2bce43af96b
je16630 <je16630@ll.mit.edu>
2011-01-24 22:30:20 +0000

    minor

--- TEST/Reuters3D4ManalysisTEST.m
+++ TEST/Reuters3D4ManalysisTEST.m
@@ -7,7 +7,7 @@
 % Script variables.
 TABLECREATE=1;  % Create new tables.
 TABLEDELETE=0;  % Delete tables after.
-NODB = 1;  % Use associative arrays instead of DB;
+NODB = 0;  % Use associative arrays instead of DB;
 LF = char(10); CR = char(13);  Q = '''';
 UNTERMINATED = 'Unknown/TBD';
 
