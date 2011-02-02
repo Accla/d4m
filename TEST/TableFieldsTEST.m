023e434512d8b57a53903b55600bc78e55dbc40b
je16630 <je16630@ll.mit.edu>
2011-02-02 02:34:37 +0000

    table fields test

---
+++ TEST/TableFieldsTEST.m
@@ -0,0 +1,26 @@
+DBsetup;  % Setup database DB.
+
+% Delete and create table.
+T = DB('DBtableFieldsTEST');
+deleteForce(T);
+T = DB('DBtableFieldsTEST');
+
+T = putSecurity(T,'FOUO');
+Security(T)
+
+putColumnFamily(T,'vertexFamily');
+ColumnFamily(T)
+
+
+%save([mfilename '.mat'],'-v6','T1r','T2r','T3r','T1c','T2c','T3c');     
+
+deleteForce(T);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% D4M: Dynamic Distributed Dimensional Data Model
+% Architect: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% Software Engineer: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% MIT Lincoln Laboratory
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) <2010> Massachusetts Institute of Technology
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
