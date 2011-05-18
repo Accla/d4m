878a3f6a885469d2ba347f1128ac5f240ee70b38
yee <yee@ll.mit.edu>
2011-05-18 22:11:43 +0000

    fix - add variable m0 and m1

--- TEST/DBtableNumLimitTEST.m
+++ TEST/DBtableNumLimitTEST.m
@@ -26,6 +26,7 @@ T = DB(tableName);
 T = putNumLimit(T,0);
 T0 = T(:,:);
 szT0 = size(T0);
+m0 = szT0(1);
 n0 = szT0(2);
 disp(['Test 0:: size of assoc array T0 =' num2str(m0) ' , '  num2str(n0)]);
 
@@ -33,6 +34,7 @@ disp(['Test 0:: size of assoc array T0 =' num2str(m0) ' , '  num2str(n0)]);
 T = putNumLimit(T,2);
 T1 = T('a ',:);
 szT1 = size(T1);
+m1 = szT1(1);
 n1 = szT1(2);
 disp(['Test 1:: size of assoc array T1 =' num2str(m1) ' , ' num2str(n1)]);
 
