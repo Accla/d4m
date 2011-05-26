ddb966adc6c1ec4abe1599d85109d40f1c254b32
yee <yee@ll.mit.edu>
2011-05-26 20:07:21 +0000

    use assert for testing

--- TEST/DBtableNumLimitTEST.m
+++ TEST/DBtableNumLimitTEST.m
@@ -29,7 +29,7 @@ szT0 = size(T0);
 m0 = szT0(1);
 n0 = szT0(2);
 disp(['Test 0:: size of assoc array T0 =' num2str(m0) ' , '  num2str(n0)]);
-
+assert (n0 == 6,'ERROR - size(2) should be 6');
 % Set limit to 2.  Get 2 results back - 1x2
 T = putNumLimit(T,2);
 T1 = T('a ',:);
@@ -37,7 +37,7 @@ szT1 = size(T1);
 m1 = szT1(1);
 n1 = szT1(2);
 disp(['Test 1:: size of assoc array T1 =' num2str(m1) ' , ' num2str(n1)]);
-
+assert (n1 == 2,'ERROR - size(2) should be 2');
 if(n0 ~= 6)
      throw(MException('ResultChk:UnexpectedResult', [' Test0- Should have gotten back 6, instead of ' n0]))
 end
