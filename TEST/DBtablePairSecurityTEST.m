a634420777276ff76fd8a7bc4717d0721feb301b
yee <yee@ll.mit.edu>
2011-06-08 23:08:20 +0000

    test for security for DBtablePair

---
+++ TEST/DBtablePairSecurityTEST.m
@@ -0,0 +1,49 @@
+AssocSetup;  % Create assoc array A.
+
+DBsetup;  % Setup database DB.
+
+% Delete and create table.
+T = DB('DBtableSecurityTEST', 'DBtableSecurityTESTt');
+deleteForce(T);
+T = DB('DBtableSecurityTEST','DBtableSecurityTESTt');
+
+try 
+    % Insert 'foo' data...
+    T = putSecurity(T,'foo');
+    put(T,A);
+
+    % Get 'foo' data... shouldn't be empty
+    T = putSecurity(T,'foo');
+    Tfoo = T('a ',:);
+
+    % Get 'bar' data... should be empty
+    T = putSecurity(T,'bar');
+    Tbar = T('a ',:);
+
+    % Delete Table
+    deleteForce(T);
+
+catch e        
+    deleteForce(T);
+    throw(e);    
+end
+
+% Check Results
+if(not(isempty(Tbar)))
+    throw(MException('ResultChk:SecurityError', '"foo" data should be inaccessable to "bar" queries'))
+end
+
+if(isempty(Tfoo))
+    throw(MException('ResultChk:DataInaccessable', '"foo" data should be accessable to "foo" queries'))
+end
+
+save([mfilename '.mat'],'-v6','Tfoo', 'Tbar');     
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% D4M: Dynamic Distributed Dimensional Data Model
+% Architect: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% Software Engineer: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% MIT Lincoln Laboratory
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) <2010> Massachusetts Institute of Technology
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
