18e19299d521c1df9b8bfffcfb234aea65ffd272
je16630 <je16630@ll.mit.edu>
2011-07-27 21:18:57 +0000

    shorthand for range quiries.

---
+++ matlab_src/StartsWith.m
@@ -0,0 +1,28 @@
+function snew = StartsSwith(s);
+%STARTSWITH creates range strings out of s.
+
+  sep = s(end);  % Get seperator.
+  smin = char(1);   smax = char(127);   % Get min and max string.
+
+  sMat = Str2mat(s);    % Create matrix.
+  sSize = size(sMat);  % Get size;
+  snewMat = char(zeros(3*sSize(1),sSize(2)+1));   % Creat new matrix.
+
+  snewMat(1:3:end,:) = Str2mat(strrep(s,sep,[smin sep]));
+  snewMat(2:3:end,1) = ':';
+  snewMat(2:3:end,2) = sep;
+  snewMat(3:3:end,:) = Str2mat(strrep(s,sep,[smax sep]));
+
+  snew = Mat2str(snewMat);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% D4M: Dynamic Distributed Dimensional Data Model
+% Architect: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% Software Engineer: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% MIT Lincoln Laboratory
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) <2010> Massachusetts Institute of Technology
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
