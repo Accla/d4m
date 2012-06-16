a320693404db7bdd43e5e48ade49e9c713ba7506
je16630 <je16630@ll.mit.edu>
2012-06-16 19:23:05 +0000

    removed timing

--- matlab_src/@DBtablePair/putTriple.m
+++ matlab_src/@DBtablePair/putTriple.m
@@ -36,14 +36,14 @@ function T = putTriple(T,r,c,v)
   DB = struct(T.DB);
 
   for i=1:chunkSize:Nr
-  insert_t = tic;
+%  insert_t = tic;
     i1 = min(i + chunkSize,Nr+1);
     rr = r((ir(i)+1):ir(i1));
     cc = c((ic(i)+1):ic(i1));
     vv = v((iv(i)+1):iv(i1));
     DBinsert(DB.instanceName, DB.host, T.name1, DB.user, DB.pass, rr, cc, vv, T.columnfamily, T.security, DB.type );
     DBinsert(DB.instanceName, DB.host, T.name2, DB.user, DB.pass, cc, rr, vv, T.columnfamily, T.security, DB.type );    % Insert transpose.
-  insert_t = toc(insert_t);  disp(['Insert time: ' num2str(insert_t)]);
+%  insert_t = toc(insert_t);  disp(['Insert time: ' num2str(insert_t)]);
   end
 
 end
