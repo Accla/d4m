7c06dd302925b199b96562585897a845a1a16d2c
Dylan Hutchison <dhutchis@stevens.edu>
2015-01-15 23:23:26 -0500

    generalize DBsetupLLGrid to allow for invocation from non-tools dir

--- matlab_src/DBsetupLLGrid.m
+++ matlab_src/DBsetupLLGrid.m
@@ -1,19 +1,30 @@
-function DB = DBsetupLLGrid(dbname)
+function DB = DBsetupLLGrid(dbname,toolspath)
 %DBsetupLLGrid: Create database binding on LLGrid.
 %Database internal function.
 %  Usage:
 %    DB = DBsetupLLGrid(group)
 %  Inputs:
 %    group = String containing name group that database lives in.
+%    toolspath = Optional input to the LLGrid tools/ directory.
+%       -Attempts to autodetect if not provided.-
 %  Outputs:
 %    DB = database binding
-
-  DBdir = fileparts(mfilename('fullpath'));   % Get DBdir.
-  fid = fopen([DBdir '/../../groups/databases/' dbname '/accumulo_user_password.txt']);
+  narginchk(1, 2)
+  fd = filesep;
+  if nargin == 1
+      DBdir = [fileparts(mfilename('fullpath')) fd '..' fd '..'];   % Get tools directory.
+  else
+      if toolspath(end) == '/' || toolspath(end) == '\'
+          toolspath = toolspath(1:end-1);
+      end
+      DBdir = toolspath;
+  end
+  fid = fopen([DBdir fd 'groups' fd 'databases' fd dbname fd 'accumulo_user_password.txt']);
+  %disp([DBdir fd 'groups' fd 'databases' fd dbname fd 'accumulo_user_password.txt'])
     AccumuloUserKey = fgetl(fid);
   fclose(fid);
 
-  fid = fopen([DBdir '/../../groups/databases/' dbname '/dnsname']);
+  fid = fopen([DBdir fd 'groups' fd 'databases' fd dbname fd 'dnsname']);
     dnsName = fgetl(fid);
   fclose(fid);
 
@@ -26,10 +37,8 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % D4M: Dynamic Distributed Dimensional Data Model
 % Architect: Dr. Jeremy Kepner (kepner@ll.mit.edu)
-% Software Engineer: Dr. Jeremy Kepner (kepner@ll.mit.edu), Dr. Vijay
-% Gadepally (vijayg@ll.mit.edu)
+% Software Engineer: Dr. Jeremy Kepner (kepner@ll.mit.edu)
 % MIT Lincoln Laboratory
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % (c) <2010> Massachusetts Institute of Technology
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
