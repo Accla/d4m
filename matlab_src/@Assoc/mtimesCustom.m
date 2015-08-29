7520748a6fb7457d8a73daeb4e2bb93d04f64e0d
Dylan Hutchison <dhutchis@mit.edu>
2015-08-29 15:26:50 -0400

    mtimesCustom reorganization.
    Move example from readme to exampels/1Intro.
    Reorder arguments in mtimesCustom.
    More doc.

--- matlab_src/@Assoc/mtimesCustom.m
+++ matlab_src/@Assoc/mtimesCustom.m
@@ -1,7 +1,42 @@
-function res = mtimesCustom(A,B,azero,afunc,mfunc)
-% azero = 0; % identity of addition operator
-% afunc = @plus;
-% mfunc = @times;
+function res = mtimesCustom(A,B,varargin) %afunc,mfunc,azero)
+%mtimesCustom: Performs matrix multiply of two associative arrays over an arbitrary ring.
+%Associative array user function.
+%  Usage:
+%    AB = mtimes(A,B)
+%    AB = mtimes(A,B,@min)           % define + as @min
+%    AB = mtimes(A,B,@min,@plus)     % define * as @plus
+%    AB = mtimes(A,B,@min,@plus,Inf) % define additive identity as Inf
+%  Inputs:
+%    A = associative array with string values (use num2str if numeric values)
+%    B = associative array with string values (use num2str if numeric values)
+%    afunc = Function to use as + (default @plus)
+%    mfunc = Function to use as * (default @times)
+%    azero = Scalar to use as the additive identity (default 0)
+%  Outputs:
+%    AB = associative array that is the matrix multiply of A and B over the given ring
+%         and limited to the intersection of Col(A) and Row(B).
+%         Please note the resulting associative array is DENSE.
+
+% TODO: Generalize to non-numeric rings.
+% TODO: Modify to support sparse output.
+% TODO: Optimize.
+
+% revert to regular mtimes in normal usage
+if nargin == 2
+    res = mtimes(A,B);
+    return
+end
+
+afunc = varargin{1};
+mfunc = @times;
+azero = 0;
+if nargin >= 4
+    mfunc = varargin{2};
+    if nargin >= 5
+        azero = varargin{3};
+    end
+end
+
 % helper:
 allButLast = @(x) x(1:end-1);
 
@@ -18,7 +53,5 @@ for i=1:size(valstrs,1)
 end
 
 Rstruct.val = result;
-res = Assoc(Rstruct);  %<-- there is no way to create an Assoc object out of its structure
-% Could modify Assoc's constructor to accept a struct.
-% Poor man's solution:
-%res = Assoc(Rstruct.row,Rstruct.col,Rstruct.val);
+res = Assoc(Rstruct);  % Creates an Associative Array directly from a structure
+%res = Assoc(Rstruct.row,Rstruct.col,Rstruct.val); % Would have to use this otherwise.
