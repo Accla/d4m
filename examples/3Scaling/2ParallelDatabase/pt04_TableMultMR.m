983f3707f8b59306409410e0116f0a8ad74716c4
Dylan Hutchison <dhutchis@cs.washington.edu>
2016-11-14 18:16:35 -0800

    initial working mr experiments

---
+++ examples/3Scaling/2ParallelDatabase/pt04_TableMultMR.m
@@ -0,0 +1,158 @@
+%DoRunMatlab = false;
+%javaaddpath /home/dhutchis/gits/lara-graphulo/target/lara-graphulo-1.0-SNAPSHOT-all.jar 
+DBsetup;
+Tinfo = DB('DH_info','DH_infoT');
+nl = char(10);
+
+for SCALE=12%10:18
+DoRunGraphulo = true;
+DoRunMR = true;%SCALE < 16; % Matlab runs out of memory at 16. 15 is tough.
+arrr = [2];
+%if SCALE==18
+%    arrr = 2;
+%end
+for NUMTAB=arrr%[1,2]%,4,8]
+fprintf('Starting TableMult SCALE=%d NUMTAB=%d\n',SCALE,NUMTAB);
+myName = ['DH_' num2str(SCALE,'%02d') '_'];
+tname = [myName 'TgraphAdj'];
+Tadj = DB(tname); %,'DH_TgraphAdjT');
+tname2 = ['DHB_' num2str(SCALE,'%02d') '_TgraphAdj'];
+Tadj2 = DB(tname2); %,'DH2_TgraphAdjT');
+rname = [tname '_t' 'X' tname2];
+mrname = [rname '_mr'];
+
+% test existence: strfind(ls(DB),[rname ' '])
+Tres = DB(rname);
+deleteForce(Tres); % Removes overhead of creating table to hold results from timing.
+Tres = DB(rname);
+
+% Split input tables if necessary.
+%G = DBaddJavaOps('edu.mit.ll.graphulo.MatlabGraphulo',INSTANCENAME,'localhost:2181','root','secret');
+tic;
+numEntries = nnz(Tadj);
+splitPoints = G.findEvenSplits(tname, NUMTAB-1, numEntries / NUMTAB);
+putSplits(Tadj, splitPoints);
+G.Compact(tname); % force new splits
+[splitPoints,splitSizes] = getSplits(Tadj);
+
+numEntries2 = nnz(Tadj2);
+splitPoints2 = G.findEvenSplits(tname2, NUMTAB-1, numEntries2 / NUMTAB);
+putSplits(Tadj2, splitPoints2);
+G.Compact(tname2); % force new splits
+[splitPoints2,splitSizes2] = getSplits(Tadj2);
+splitCompact = toc; fprintf('Both Split %d & compact time: %f\n',NUMTAB,splitCompact);
+
+% Pre-splitting
+UseBestSplitsR = false;
+if UseBestSplitsR && NUMTAB > 1
+    row = [rname '_nt' num2str(NUMTAB) nl];
+    splitPointsRBest = Val(Tinfo(row,'splitPointsRBest,'));
+    splitPointsR = splitPointsRBest;
+    putSplits(Tres,splitPointsR);
+else
+    putSplits(Tres,splitPoints); % arbitrary between 1 and 2
+    splitPointsR = splitPoints;
+end
+G.Compact(getName(Tres));
+
+pause(3)
+
+if DoRunGraphulo
+tic;
+presumCacheSize = -1;
+numpp = G.TableMult(tname,tname2,rname,'','','','',presumCacheSize,-1,false);
+graphuloMult = toc; fprintf('Graphulo TableMult Time: %f\n',graphuloMult);
+fprintf('Result Table %s #entries: %d\n',rname,nnz(Tres));
+
+[splitPointsR,splitSizesR] = getSplits(Tres);
+pause(3)
+end
+
+
+
+if DoRunMR
+TresMat = DB(mrname);
+deleteForce(TresMat);
+TresMat = DB(mrname);
+putSplits(TresMat,splitPointsR);
+G.Compact(getName(TresMat));
+pause(3)
+
+if 0
+javaClass = 'edu.washington.cs.laragraphulo.mr.MatMulJob';
+[~, jPath, jFunc] = fileparts(javaClass);
+import([jPath '.*']);
+MR = feval(jFunc(2:end));
+%,'-i', 'accumulo-1.8', '-z', 'localhost:2181', '-t1', tname, '-t2', tname2, '-o', mrname, '-u', 'secret', '-p', 'root', '--reducers', '1');
+javaClass = 'org.apache.hadoop.util.ToolRunner';
+[~, jPath, jFunc] = fileparts(javaClass);
+import([jPath '.*']);
+TR = feval(jFunc(2:end));
+javaClass = 'org.apache.hadoop.conf.Configuration';
+[~, jPath, jFunc] = fileparts(javaClass);
+import([jPath '.*']);
+CONF = feval(jFunc(2:end));
+
+ars0 = {'-i', 'accumulo-1.8', '-z', 'localhost:2181', '-t1', tname, '-t2', tname2, '-o', mrname, '-u', 'root', '-p', 'secret', '--reducers', '1', '--noDelete'};
+ars = javaArray('java.lang.String',length(ars0));
+for i = 1:length(ars0)
+    ars(i) = java.lang.String(ars0{i});
+end
+end
+
+tic;
+%TR.run(CONF, MR, ars);
+system(['$ACCUMULO_HOME/bin/tool.sh /home/dhutchis/gits/lara-graphulo/target/lara-graphulo-1.0-SNAPSHOT-all.jar edu.washington.cs.laragraphulo.mr.MatMulJob -i accumulo-1.8 -z localhost:2181 -t1 ' tname ' -t2 ' tname2 ' -o ' mrname ' -u root -p secret --reducers 1 --noDelete'], '-echo')
+mrTime = toc; fprintf('MR Time: %f\n',mrTime);
+
+% Check correctness
+% Disabled because out of memory error gathering all the result table in Matlab memory
+if 0
+AtBDB = str2num(Tres(:,:));
+correct = isequal(AtB,AtBDB);
+if ~correct
+    fprintf('NOT EQUAL RESULTS LOCAL AND DB VERSION\n');
+    %return
+end
+end
+end
+
+% Record number of partial products to determine rate
+%numpp = G.countPartialProductsTableMult(tname,tname2,false);
+
+if exist('Tinfo','var')
+    row = [rname '_nt' num2str(NUMTAB) nl];
+    Ainfo = Assoc('','','');
+    if DoRunGraphulo
+        Ainfo = Ainfo + Assoc(row,['graphuloMult' nl],[num2str(graphuloMult) nl]);
+    end
+    if DoRunMR
+        Ainfo = Ainfo + Assoc(row,['mrTime' nl],[num2str(mrTime) nl]);
+    end
+    %Ainfo = Ainfo + Assoc(row,['correct' nl],[num2str(correct) nl]);
+    Ainfo = Ainfo + Assoc(row,['numpp' nl],[num2str(numpp) nl]);
+    if (NUMTAB > 1)
+        Ainfo = Ainfo + Assoc(row,['splitPoints' nl],[splitPoints nl]);
+        Ainfo = Ainfo + Assoc(row,['splitSizes' nl],[splitSizes nl]);
+        Ainfo = Ainfo + Assoc(row,['splitPoints2' nl],[splitPoints2 nl]);
+        Ainfo = Ainfo + Assoc(row,['splitSizes2' nl],[splitSizes2 nl]);
+        Ainfo = Ainfo + Assoc(row,['splitPointsR' nl],[splitPointsR nl]);
+        Ainfo = Ainfo + Assoc(row,['splitSizesR' nl],[splitSizesR nl]);
+    end
+    Ainfo = Ainfo + Assoc(row,['splitCompact' nl],[num2str(splitCompact) nl]);
+    put(Tinfo,Ainfo);
+end
+
+pause(10)
+
+end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% D4M: Dynamic Distributed Dimensional Data Model
+% Architect: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% Software Engineer: Dylan Hutchison (dhutchis@mit.edu)
+% MIT Lincoln Laboratory
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) <2015> Massachusetts Institute of Technology
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ examples/3Scaling/2ParallelDatabase/pt05_PlotMR.m
@@ -0,0 +1,193 @@
+DBsetup;
+Tinfo = DB('DH_info','DH_infoT');
+nl = char(10);
+
+xSCALE = 12;
+aNUMTAB = [1,2].';%,4,8].';
+yTimeGraphulo = zeros(numel(aNUMTAB),numel(xSCALE));
+yTimeD4M = zeros(numel(aNUMTAB),numel(xSCALE));
+yRateGraphulo = zeros(numel(aNUMTAB),numel(xSCALE));
+yRateD4M = zeros(numel(aNUMTAB),numel(xSCALE));
+yPP = zeros(1,numel(xSCALE));
+
+for iNUMTAB=1:numel(aNUMTAB)
+NUMTAB=aNUMTAB(iNUMTAB);
+for iSCALE=1:numel(xSCALE)
+SCALE=xSCALE(iSCALE);
+tname = ['DH_' num2str(SCALE,'%02d') '_TgraphAdj'];
+tname2 = ['DHB_' num2str(SCALE,'%02d') '_TgraphAdj'];
+rname = [tname '_t' 'X' tname2]; 
+mrname = [rname '_mr'];
+row = [rname '_nt' num2str(NUMTAB) nl];
+numpp = Val(str2num(Tinfo(row,'numpp,')));
+yPP(iSCALE) = numpp;
+gra = Val(str2num(Tinfo(row,'graphuloMult,')));
+if isempty(gra)
+    gra = 0;
+    yRateGraphulo(iNUMTAB,iSCALE) = 0;
+else
+    yRateGraphulo(iNUMTAB,iSCALE) = numpp ./ gra;
+end
+yTimeGraphulo(iNUMTAB,iSCALE) = gra;
+d4m = Val(sum(str2num(Tinfo(row,'mrTime,')),2));
+if isempty(d4m)
+    d4m = 0;
+    yRateD4M(iNUMTAB,iSCALE) = 0;
+else
+    yRateD4M(iNUMTAB,iSCALE) = numpp ./ d4m;
+end
+yTimeD4M(iNUMTAB,iSCALE) = d4m;
+end
+end
+
+legs = cell(1,2*numel(aNUMTAB));
+figure
+hold on
+for iNUMTAB=1:numel(aNUMTAB)
+grarow = yTimeGraphulo(iNUMTAB,:);
+grarow = log2(grarow);
+plot(xSCALE(grarow~=0),grarow(grarow~=0),'.-')
+msg = ['Graphulo ' num2str(iNUMTAB) ' Tablet'];
+if aNUMTAB(iNUMTAB) > 1
+    msg = [msg 's'];
+end
+legs{iNUMTAB} = msg;
+end
+for iNUMTAB=1:numel(aNUMTAB)
+d4mrow = yTimeD4M(iNUMTAB,:);
+d4mrow = log2(d4mrow);
+%fprintf('d4mrow %d\n',d4mrow);
+plot(xSCALE(d4mrow~=0),d4mrow(d4mrow~=0),'--.')
+msg = ['MapReduce ' num2str(aNUMTAB(iNUMTAB)) ' Tablet'];
+if aNUMTAB(iNUMTAB) > 1
+    msg = [msg 's'];
+end
+legs{numel(aNUMTAB)+iNUMTAB} = msg;
+end
+%ax = gca;
+%ax.XTick = 10:13;
+legend(legs);
+xlabel('SCALE');
+ylabel('log_2( Time (s) )');
+axis([-inf,+inf,0,+inf])
+title('TableMult Runtime Scaling');
+savefig('TableMultTime');
+print('TableMultTime','-depsc')
+print('TableMultTime','-dpng')
+
+legs = cell(1,2*numel(aNUMTAB));
+figure
+gca.XTick=xSCALE;
+hold on
+for iNUMTAB=1:numel(aNUMTAB)
+grarow = yRateGraphulo(iNUMTAB,:);
+plot(xSCALE(grarow~=0),grarow(grarow~=0),'.-')
+msg = ['Graphulo ' num2str(aNUMTAB(iNUMTAB)) ' Tablet'];
+if aNUMTAB(iNUMTAB) > 1
+    msg = [msg 's'];
+end
+legs{iNUMTAB} = msg;
+end
+for iNUMTAB=1:numel(aNUMTAB)
+d4mrow = yRateD4M(iNUMTAB,:);
+%disp(d4mrow)
+plot(xSCALE(d4mrow~=0),d4mrow(d4mrow~=0),'--.')
+msg = ['MapReduce ' num2str(aNUMTAB(iNUMTAB)) ' Tablet'];
+if aNUMTAB(iNUMTAB) > 1
+    msg = [msg 's'];
+end
+legs{numel(aNUMTAB)+iNUMTAB}   = msg;
+end
+%ax.XTick = 10:13;
+legend(legs);
+xlabel('SCALE');
+ylabel('Rate (partial products/s)');
+title('TableMult Rate Scaling');
+axis([-inf,+inf,0,+inf])
+savefig('TableMultRate');
+print('TableMultRate','-depsc')
+print('TableMultRate','-dpng')
+
+
+% Compact all
+if 0
+for iSCALE=8:9%1:numel(xSCALE)
+for iNUMTAB=2:numel(aNUMTAB)
+NUMTAB=aNUMTAB(iNUMTAB);
+SCALE=xSCALE(iSCALE);
+tname = ['DH_' num2str(SCALE,'%02d') '_TgraphAdj'];
+tname2 = ['DHB_' num2str(SCALE,'%02d') '_TgraphAdj'];
+rname = [tname '_t' 'X' tname2];
+row = [rname '_nt' num2str(NUMTAB) nl];
+G = DBaddJavaOps('edu.mit.ll.graphulo.MatlabGraphulo','instance','localhost:2181','root','secret');
+G.Compact(rname);
+
+% Get best split point for result table
+Tres = DB(rname);
+numEntries = nnz(Tres);
+splitPoints = G.findEvenSplits(rname, NUMTAB-1, numEntries / NUMTAB);
+putSplits(Tres, splitPoints);
+G.Compact(rname);
+[splitPoints,splitSizes] = getSplits(Tres);
+Ainfo = Assoc('','','');
+Ainfo = Ainfo + Assoc(row,'splitPointsRBest,',[splitPoints nl]);
+Ainfo = Ainfo + Assoc(row,'splitSizesRBest,',[splitSizes nl]);
+Ainfo = Ainfo + Assoc(row,'nnz,',[num2str(numEntries) nl]);
+put(Tinfo,Ainfo);
+fprintf('NUMTAB %d SCALE %d \n R     %s RBest %s R     %s RBest %s\n',NUMTAB,SCALE,Val(Tinfo(row,'splitPointsR,')),...
+        Val(Tinfo(row,'splitPointsRBest,')),Val(Tinfo(row,'splitSizesR,')),Val(Tinfo(row,'splitSizesRBest,')));
+end
+end
+end
+
+yNNZ = zeros(1,numel(xSCALE));
+
+% Other analyses
+for iSCALE=1:numel(xSCALE)
+for iNUMTAB=2:numel(aNUMTAB) % skip first
+NUMTAB=aNUMTAB(iNUMTAB);
+SCALE=xSCALE(iSCALE);
+tname = ['DH_' num2str(SCALE,'%02d') '_TgraphAdj'];
+tname2 = ['DHB_' num2str(SCALE,'%02d') '_TgraphAdj'];
+rname = [tname '_t' 'X' tname2];
+row = [rname '_nt' num2str(NUMTAB) nl];
+numpp = Val(str2num(Tinfo(row,'numpp,')));
+md = maxdiff(Val(Tinfo(row,'splitSizesR,')));
+yNNZ(iSCALE) = Val(str2num(Tinfo(row,'nnz,')));
+fprintf('NUMTAB %d SCALE %d MaxDiff %9d NumPP %9d nnzCompact %9d\n',NUMTAB,SCALE,md,numpp,yNNZ(iSCALE));
+end
+end
+
+%for iSCALE=1:numel(xSCALE)
+%    yPP(iSCALE) = 
+%end
+
+a = [xSCALE; yPP; yNNZ; yTimeGraphulo(1,:); yRateGraphulo(1,:); yTimeD4M(1,:); yRateD4M(1,:); yTimeGraphulo(2,:); yRateGraphulo(2,:); yTimeD4M(2,:); yRateD4M(2,:)];
+% & & & \multicolumn{2}{|c|}{Graphulo 1 Tablet} & \multicolumn{2}{|c|}{D4M 1 Tablet} & \multicolumn{2}{|c|}{Graphulo 2 Tablets} & \multicolumn{2}{|c|}{D4M 2 Tablets} \\
+% Using: https://www.mathworks.com/matlabcentral/answers/96131-is-there-a-format-in-matlab-to-display-numbers-such-that-commas-are-automatically-inserted-into-the
+%b = a;%num2bank(a);
+b = arrayfun(@addmatlatex, a, 'UniformOutput', false);
+%b = arrayfun(@(x) num2str(x,'%f') , a, 'UniformOutput', false);
+%b = cellfun(@elim0,b,'UniformOutput',false);
+%b = cellfun(@addmatlatex,b,'UniformOutput',false);
+% Using: https://www.mathworks.com/matlabcentral/fileexchange/44274-converting-matlab-data-to-latex-table
+inp = struct();
+inp.data = b;
+inp.tableRowLabels = {'SCALE','\#PartialProducts','nnz(C)','Time (s)','Rate','Time (s)','Rate','Time (s)','Rate','Time (s)','Rate'};
+inp.tableCaption = 'Numerical results and parameters for Figure~\ref{fTableMultPerf}';
+inp.tableLabel = 'lResultsParams';
+inp.dataFormat = {'%s'};
+inp.transposeTable = true;
+inp.tableColumnAlignment = 'l';
+lat = latexTable(inp)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% D4M: Dynamic Distributed Dimensional Data Model
+% Architect: Dr. Jeremy Kepner (kepner@ll.mit.edu)
+% Software Engineer: Dylan Hutchison (dhutchis@mit.edu)
+% MIT Lincoln Laboratory
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (c) <2015> Massachusetts Institute of Technology
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
