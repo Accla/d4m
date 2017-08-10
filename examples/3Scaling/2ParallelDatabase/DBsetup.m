1dd8cc56089b93ca40f8860d64741b37cdeba207
Lauren Milechin <13963460+lmilechin@users.noreply.github.com>
2017-08-10 14:49:03 -0400

    Update DBsetup.m

--- examples/3Scaling/2ParallelDatabase/DBsetup.m
+++ examples/3Scaling/2ParallelDatabase/DBsetup.m
@@ -9,7 +9,7 @@ end
 %INSTANCENAME = 'classdb54';
 %[DB,G] = DBsetupLLGrid('classdb54', '/home/gridsan/');                                      % Create binding to database.  Shorthand for:
 INSTANCENAME = 'instance-1.7.0';
-DB = DBserver('localhost:2181','Accumulo',INSTANCENAME,'root','secret');		
+DB = DBserver('localhost:2181','Accumulo',INSTANCENAME,'root','secret');
 G = DBaddJavaOps('edu.mit.ll.graphulo.MatlabGraphulo',INSTANCENAME,'localhost:2181','root','secret');
 % DB = DBserver('llgrid-db-00.llgrid.ll.mit.edu:2181','Accumulo','accumulo','AccumuloUser',password);
 % Check status of database at http://llgrid-db-00.llgrid.ll.mit.edu:50095                                                
