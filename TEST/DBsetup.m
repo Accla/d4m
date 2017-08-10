426b5a6f3b0cac04d350460809fb2e8ff234785e
Lauren Milechin <13963460+lmilechin@users.noreply.github.com>
2017-08-10 14:17:03 -0400

    Update DBsetup.m

--- TEST/DBsetup.m
+++ TEST/DBsetup.m
@@ -3,7 +3,7 @@ if not(exist('DB', 'var'))
     
     % Create a DB.  
     %DB = DBserver('classdb01.cloud.llgrid.ll.mit.edu:2181','Accumulo','accumulo',user,password);
-    DB = DBsetupLLGrid('txg-graphulotest-02');
+    DB = DBserver('localhost:2181','Accumulo','instance','root','secret');
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
